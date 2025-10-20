48b569407cd397339595b2ffbcc72003d2aeb310
dariop14-cell <dariopetasny@gmail.com>
2025-10-20 11:00:58 -0300

    avances20deOct

--- src/app/navbar/nabvar.component.ts
+++ src/app/navbar/nabvar.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nabvar',
-  imports: [RouterLink],
+  imports: [RouterLink,CommonModule],
   templateUrl: './nabvar.component.html',
   styleUrl: './nabvar.component.css'
 })
 export class NabvarComponent {
+  mostrarModal: boolean = false;
+
+  abrirModal(){
+    this.mostrarModal = true;
+  }
+
+  cerrarModal(){
+    this.mostrarModal = false;
+  }
 }
